c54cc4f7584fa683f9515640768f294b3497a6fb
alexander swedlund <alexander.swedlund@elev.ga.ntig.se>
2024-10-15 14:34:27 +0200

    klmkmlk

---
+++ recruitment/Program.cs
@@ -0,0 +1,33 @@
+﻿string enemyName = "";
+
+List<string> names = ["buster", "terry", "tom", "jerry"];
+List<string> recruits = new List<string>();
+
+while (names.Count > 2)
+{
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine("choices");
+        Console.WriteLine(names[i]);
+    }
+    Console.WriteLine();
+    Console.WriteLine("choose 1");
+   string choice = Console.ReadLine();
+    for (int i = 0; i < recruits.Count; i++)
+    {
+        Console.WriteLine("gang");
+        Console.WriteLine(recruits[i]);
+    }
+    Console.WriteLine();
+    
+}
+
+//names.Add ("aaadd");
+//names.Remove("crazy");
+//names.RemoveAt(2);
+//names[0]= "alexander";
+//
+//int i = Random.Shared.Next(names.Count);
+//Console.WriteLine(names[i]);
+
+Console.ReadLine();
